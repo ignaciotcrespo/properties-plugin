6e66d532f71e1ee9f8345f7d0eef055e5ecbcda9
Ignacio Tomas Crespo <itcrespo@gmail.com>
2018-05-10 21:10:11 -0300

    double click to open properties file

--- src/com/github/ignaciotcrespo/ags/GradleSwitchesToolWindowFactory.java
+++ src/com/github/ignaciotcrespo/ags/GradleSwitchesToolWindowFactory.java
@@ -1,5 +1,8 @@
 package com.github.ignaciotcrespo.ags;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
@@ -11,22 +14,27 @@ import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import com.intellij.ui.table.JBTable;
+import io.reactivex.Emitter;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.*;
 import java.util.List;
-import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 public class GradleSwitchesToolWindowFactory implements ToolWindowFactory {
 
-    private JPanel panel;
 
     static class Item {
         String name;
@@ -40,174 +48,205 @@ public class GradleSwitchesToolWindowFactory implements ToolWindowFactory {
         }
     }
 
+    static class ValidFile {
+        File file;
+        String projectRootFolder;
+
+        public ValidFile(File file, String projectRootFolder) {
+            this.file = file;
+            this.projectRootFolder = projectRootFolder;
+        }
+
+        public String getRelativePath() {
+            return file.toString().substring(projectRootFolder.length());
+        }
+    }
+
+
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
 
-//        Observable.create(new ObservableOnSubscribe<Item>() {
-//            @Override
-//            public void subscribe(ObservableEmitter<Item> emitter) throws Exception {
-//                Module[] modules = ModuleManager.getInstance(project).getModules();
-//                VirtualFile file = ModuleRootManager.getInstance(modules[0]).getContentRoots()[0];
-//                List<VirtualFile> allExcludedRoots = new ArrayList<>();
-//                for (Module module: modules) {
-//                    VirtualFile[] excludedRoots = ModuleRootManager.getInstance(module).getExcludeRoots();
-//                    allExcludedRoots.addAll(Arrays.asList(excludedRoots));
-//
-////            for (VirtualFile f: excludedRoots) {
-////                names.add(f.toString());
-////                values.add("excluded");
-////                filePaths.add("meh");
-////            }
-//                }
-//                String projectRootFolder = file.getCanonicalPath();
-//                File file1 = new File(projectRootFolder);
-//                searchProperties(emitter, file1, projectRootFolder, allExcludedRoots);
-//            }
-//        });
+        List<Object> items = new ArrayList<>();
 
-        Observable.fromCallable(new Callable<JScrollPane>() {
+        PublishSubject<MouseEvent> clicks = PublishSubject.create();
+
+        //JTable table = new TreeTable(new ListTreeTableModel());
+        JBTable table = new JBTable();
+        table.setExpandableItemsEnabled(true);
+        DefaultTableModel tableModel = new PropsTableModel();
+        tableModel.setColumnIdentifiers(new Object[]{"File", "Name", "Value"});
+
+        table.setModel(tableModel);
+
+        table.addMouseListener(new java.awt.event.MouseAdapter() {
             @Override
-            public JScrollPane call() throws Exception {
-                List<String> names = new ArrayList<>();
-                List<String> values = new ArrayList<>();
-                List<String> filePaths = new ArrayList<>();
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                clicks.onNext(evt);
+            }
+        });
+        clicks
+                .throttleLast(250, TimeUnit.MILLISECONDS)
+                .subscribe(evt -> {
+                    int row = table.rowAtPoint(evt.getPoint());
+                    int col = table.columnAtPoint(evt.getPoint());
+                    if (row >= 0 && col >= 0) {
+//                    Object value = table.getModel().getValueAt(row, col);
+                        Object item = items.get(row);
+                        if (item instanceof ValidFile) {
+                            openValidFile(project, table, (ValidFile) item);
+                        } else {
+                            while (--row >= 0) {
+                                item = items.get(row);
+                                if (item instanceof ValidFile) {
+                                    openValidFile(project, table, (ValidFile) item);
+                                    break;
+                                }
+                            }
+                        }
 
+                    }
+                });
+
+
+        JScrollPane tableContainer = new JBScrollPane(table);
+        tableContainer.setLayout(new ScrollPaneLayout());
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        JToolBar toolBar = new JToolBar();
+        JButton button = new JButton();
+        button.setText("Refresh");
+        button.addActionListener(actionEvent -> {
+            refresh(project, tableModel, items);
+        });
+        toolBar.add(button);
+        toolBar.setFloatable(false);
+        panel.add(toolBar, BorderLayout.NORTH);
+
+        panel.add(tableContainer, BorderLayout.CENTER);
+
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(panel, "", false);
+
+
+        toolWindow.getContentManager().addContent(content);
+
+
+        refresh(project, tableModel, items);
+
+
+    }
+
+    private void openValidFile(@NotNull Project project, JBTable table, ValidFile item) {
+        VirtualFile fileByIoFile = LocalFileSystem.getInstance().findFileByIoFile(item.file);
+        ApplicationManager.getApplication().invokeLater(() -> {
+            FileEditorManager.getInstance(project).openFile(fileByIoFile, true, true);
+        }, ModalityState.stateForComponent(table));
+    }
+
+    private void refresh(@NotNull Project project, DefaultTableModel tableModel, List<Object> items) {
+        getValidFilesObservable(project)
+                .subscribeOn(Schedulers.io())
+                .doOnNext(f -> {
+                    items.add(f);
+                    tableModel.addRow(new Object[]{f.getRelativePath()});
+                })
+                .flatMap(this::getItemsObservable)
+                .doOnNext(item -> {
+                    items.add(item);
+                    tableModel.addRow(new Object[]{"", item.name, item.value});
+                    tableModel.fireTableDataChanged();
+                }).doOnSubscribe(disposable -> {
+            items.clear();
+            clearTable(tableModel);
+        })
+                .subscribe();
+    }
+
+    private void clearTable(DefaultTableModel tableModel) {
+        while (tableModel.getRowCount() > 0) {
+            tableModel.removeRow(0);
+        }
+        tableModel.fireTableDataChanged();
+    }
+
+    private Observable<Item> getItemsObservable(ValidFile f) {
+        return Observable.create(emitter -> {
+            searchProperties(emitter, f);
+            emitter.onComplete();
+        });
+    }
+
+    private Observable<ValidFile> getValidFilesObservable(@NotNull Project project) {
+        return Observable.create((ObservableEmitter<ValidFile> emitter) -> {
+            try {
                 Module[] modules = ModuleManager.getInstance(project).getModules();
                 VirtualFile file = ModuleRootManager.getInstance(modules[0]).getContentRoots()[0];
                 List<VirtualFile> allExcludedRoots = new ArrayList<>();
-                for (Module module: modules) {
+                for (Module module : modules) {
                     VirtualFile[] excludedRoots = ModuleRootManager.getInstance(module).getExcludeRoots();
                     allExcludedRoots.addAll(Arrays.asList(excludedRoots));
-
-//            for (VirtualFile f: excludedRoots) {
-//                names.add(f.toString());
-//                values.add("excluded");
-//                filePaths.add("meh");
-//            }
                 }
                 String projectRootFolder = file.getCanonicalPath();
                 File file1 = new File(projectRootFolder);
-                searchProperties(names, values, filePaths, file1, projectRootFolder, allExcludedRoots);
-
-                String[] columnNames = {"Name", "Value", "File"};
-                Object[][] data = new Object[names.size()][3];
-                for (int i = 0; i < names.size(); i++) {
-                    data[i] = new String[]{names.get(i), values.get(i), filePaths.get(i)};
-                }
-                //{{projectRootFolder, "Smith"},{"John", "Doe"}};
-                JTable table = new JTable(data, columnNames);
-                table.setFillsViewportHeight(true);
-
-
-
-//        ListTableModel<String> data = new ListTableModel<>();
-//        data.addRow("uno");
-//        data.addRow("dos");
-//        TableColumnModel columnNames = new DefaultTableColumnModel();
-//        TableColumn tableColumn = new TableColumn();
-//        tableColumn.setHeaderValue("titulazo!");
-//        columnNames.addColumn(tableColumn);
-//        JBTable table = new JBTable(data, columnNames);
-//        table.setAutoResizeMode(JBTable.AUTO_RESIZE_ALL_COLUMNS);
-//        table.setAutoCreateColumnsFromModel(true);
-//        table.getEmptyText().setText("empty!!");
-//        table.setFillsViewportHeight(true);
-
-                JScrollPane tableContainer = new JBScrollPane(table);
-                tableContainer.setLayout(new ScrollPaneLayout());
-
-                panel = new JPanel();
-                panel.add(tableContainer, BorderLayout.CENTER);
-//        frame.getContentPane().add(panel);
-//
-//        frame.pack();
-//        frame.setVisible(true);
-                return tableContainer;
+                searchFiles(emitter, file1, projectRootFolder, allExcludedRoots);
+            } finally {
+                emitter.onComplete();
             }
-        })
-//                .observeOn(Schedulers.trampoline())
-//                .subscribeOn(Schedulers.newThread())
-        .subscribe(tableContainer -> {
-            ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-            Content content = contentFactory.createContent(tableContainer, "", false);
-
-
-            toolWindow.getContentManager().addContent(content);
-
-        }, System.out::println)
-        ;
-
-
-
-
+        });
     }
 
-    private void searchProperties(ObservableEmitter<Item> emitter, File folder, String projectRootFolder, List<VirtualFile> excludedRoots) {
+    private void searchProperties(Emitter<Item> emitter, ValidFile file) {
+        Properties props = new Properties();
+        try {
+            props.load(new FileInputStream(file.file));
+            Enumeration<?> propertyNames = props.propertyNames();
+            Map<String, Item> map = new TreeMap<>();
+            while (propertyNames.hasMoreElements()) {
+                String key = propertyNames.nextElement().toString();
+                Item item = new Item(key, props.getProperty(key), file.file.toString().substring(file.projectRootFolder.length()));
+                map.put(key, item);
+            }
+            for (Item item : map.values()) {
+                emitter.onNext(item);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            emitter.onComplete();
+        }
+    }
+
+    private void searchFiles(ObservableEmitter<ValidFile> emitter, File folder, String projectRootFolder, List<VirtualFile> excludedRoots) {
         File[] files = folder.listFiles();
-        if(files != null) {
-            for (File f: files) {
-                if(f.isDirectory()){
+        if (files != null) {
+            for (File f : files) {
+                if (f.isDirectory()) {
                     VirtualFile fVirtual = LocalFileSystem.getInstance().findFileByIoFile(f);
                     boolean isExcluded = false;
-                    for (VirtualFile excluded: excludedRoots) {
-                        if(excluded.equals(fVirtual)){
+                    for (VirtualFile excluded : excludedRoots) {
+                        if (excluded.equals(fVirtual)) {
                             isExcluded = true;
                             break;
                         }
                     }
-                    if(!isExcluded) {
-                        searchProperties(emitter, f, projectRootFolder, excludedRoots);
+                    if (!isExcluded) {
+                        searchFiles(emitter, f, projectRootFolder, excludedRoots);
                     }
-                } else if(f.toString().endsWith(".properties") || f.getName().equals("settings.gradle")){
-                    Properties props = new Properties();
-                    try {
-                        props.load(new FileInputStream(f));
-                        Enumeration<?> propertyNames = props.propertyNames();
-                        while (propertyNames.hasMoreElements()) {
-                            String key = propertyNames.nextElement().toString();
-                            emitter.onNext(new Item(key, props.getProperty(key), f.toString().substring(projectRootFolder.length())));
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                } else if (f.toString().endsWith(".properties") || f.getName().equals("settings.gradle")) {
+                    emitter.onNext(new ValidFile(f, projectRootFolder));
                 }
             }
         }
     }
 
-    private void searchProperties(List<String> names, List<String> values, List<String> filePaths, File folder, String projectRootFolder, List<VirtualFile> excludedRoots) {
-        File[] files = folder.listFiles();
-        if(files != null) {
-            for (File f: files) {
-                if(f.isDirectory()){
-                    VirtualFile fVirtual = LocalFileSystem.getInstance().findFileByIoFile(f);
-                    boolean isExcluded = false;
-                    for (VirtualFile excluded: excludedRoots) {
-                        if(excluded.equals(fVirtual)){
-                            isExcluded = true;
-                            break;
-                        }
-                    }
-                    if(!isExcluded) {
-                        searchProperties(names, values, filePaths, f, projectRootFolder, excludedRoots);
-                    }
-                } else if(f.toString().endsWith(".properties") || f.getName().equals("settings.gradle")){
-                    Properties props = new Properties();
-                    try {
-                        props.load(new FileInputStream(f));
-                        Enumeration<?> propertyNames = props.propertyNames();
-                        while (propertyNames.hasMoreElements()) {
-                            String key = propertyNames.nextElement().toString();
-                            names.add(key);
-                            values.add(props.getProperty(key));
-                            filePaths.add(f.toString().substring(projectRootFolder.length()));
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
+
+    private static class PropsTableModel extends DefaultTableModel {
+        @Override
+        public boolean isCellEditable(int row, int col) {
+            return false;
+//                if (col == 0) return false;
+//                return super.isCellEditable(row, col);
         }
     }
-
 }
